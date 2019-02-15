fb700367b2a87eadce491e32a7ad8099bd72127c
palstorsveen <k01166@p1mac069.local>
2019-02-15 09:14:06 +0100

    gcloud test

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const app = express();
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'storsveen.rocks.mysql',
-    user: 'storsveen_rocks',
-    password: 'mbKesMfP1',
-    database: 'storsveen_rocks',
-});
-
-connection.connect();
-
 app.get('/', function (request, response) {
-    let result = connection.query('SELECT * FROM `IntrepName`', function (err, rows, fields) {
-        if(err) throw err;
-        result = rows;
-    });
+    let result = {
+        hey: 'test',
+        eyo: 'test2'
+    }
 
     response.send(result);
 });
 
-app.listen(8080, () => {
-    console.log('listening on port 8080');
-})
+
+const server = app.listen(8080, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log(`Example app listening at http://${host}:${port}`);
+});
