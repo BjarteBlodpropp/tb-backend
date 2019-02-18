114cc59f2f3c53d7bef60c670b2f63f5fb4ee2b9
palstorsveen <k01166@p1mac069.local>
2019-02-18 08:18:03 +0100

    testing

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database');
 
 app.get('/', function (request, response) {
-    let result = {
-        hey: 'test',
-        eyo: 'test2'
-    }
-
-    response.send(result);
+    connection.query('SELECT * FROM timebanken.timelogs', function(err, res, fields) {
+        if(err) throw err;
+        response.json(res);
+    });
 });
 
 
 const server = app.listen(8080, () => {
     const host = server.address().address;
     const port = server.address().port;
+    console.log("host:" + process.env.DB_HOST);
 
     console.log(`Example app listening at http://${host}:${port}`);
 });
