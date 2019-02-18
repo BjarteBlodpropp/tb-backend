16171f70953070843a140482c5050ab881e62584
palstorsveen <k01166@p1mac069.local>
2019-02-18 10:40:32 +0100

    testing production

--- database.js
+++ database.js
@@ -1,11 +1,26 @@
 const mysql = require('mysql');
 
-let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS
-});
+
+if(process.env.ENV && process.env.ENV == "dev") {
+    console.log("creating db connection for development");
+    let connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASS
+    });
+} else {
+    console.log("creating db connection for production");
+    let config = {
+        user: process.env.SQL_USER,
+        database: process.env.SQL_DATABASE,
+        password: process.env.SQL_PASSWORD,
+    }
+
+    if (process.env.INSTANCE_CONNECTION_NAME) {
+        config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+    }
+}
 
 connection.connect(function (err) {
     if(err) {
